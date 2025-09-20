2290049201e7dd80e6dde6fb6ffc861a6de91b7e
qulDev <mm.rizzqullah@gmail.com>
2025-09-20 23:32:28 +0800

    feat: refactor main function to initialize database, cache, and routes

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,9 +1,35 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/qulDev/jwt-gin-gorm/internal/config"
+	"github.com/qulDev/jwt-gin-gorm/internal/handler"
+	"github.com/qulDev/jwt-gin-gorm/internal/migration"
+	"github.com/qulDev/jwt-gin-gorm/internal/repository"
+	"github.com/qulDev/jwt-gin-gorm/internal/routes"
+	"github.com/qulDev/jwt-gin-gorm/internal/service"
+)
 
 func main() {
-	route := gin.Default()
+	// Load environment variables
+	config.LoadEnv()
 
-	route.Run(":8080")
+	// Initialize database and cache
+	db := config.InitDB()
+	cache := config.InitCache()
+
+	// Run database migrations
+	migration.Migrate(db)
+
+	_ = cache // to avoid unused variable error, remove when cache is used
+
+	userRepo := repository.NewUserRepository(db)
+	userService := service.NewUserService(userRepo)
+	userHendler := handler.NewUserHandler(userService)
+
+	// Initialize Gin router
+	r := routes.SetupRoutes(&routes.Handlers{
+		User: userHendler,
+	})
+
+	r.Run(":8080")
 }
