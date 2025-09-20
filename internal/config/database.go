42fe29dd08e951fbef3b7780be9aa829f239687b
qulDev <mm.rizzqullah@gmail.com>
2025-09-20 23:32:51 +0800

    feat: add database initialization function using GORM

---
+++ internal/config/database.go
@@ -0,0 +1,20 @@
+package config
+
+import (
+	"log"
+	"os"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+func InitDB() *gorm.DB {
+	dsn := os.Getenv("POSTGRES_URL")
+
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Fatal("failed to connect database: ", err)
+	}
+
+	return db
+}
