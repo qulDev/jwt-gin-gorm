5b035f4ac700b706773cf120dc7d66f8c30abdf0
qulDev <mm.rizzqullah@gmail.com>
2025-09-20 19:11:54 +0800

    fix: handle error in HashPassword function

--- pkg/hash/hash.go
+++ pkg/hash/hash.go
@@ -2,13 +2,14 @@ package hash
 
 import "golang.org/x/crypto/bcrypt"
 
-
 // HashPassword hashes a plain text password using bcrypt.
 func HashPassword(password string) (string, error) {
 	bytes, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	if err != nil {
+		return "", err
+	}
 	return string(bytes), err
 }
-
 
 // CheckPasswordHash compares a plain text password with a hashed password.
 func CheckPasswordHash(password, hash string) bool {
