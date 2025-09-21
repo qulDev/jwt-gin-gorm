6e2ea84bfa5a446912da4f240b76cdf117738558
qulDev <mm.rizzqullah@gmail.com>
2025-09-21 16:19:32 +0800

    feat: add article route setup to API version 1

--- internal/routes/routes.go
+++ internal/routes/routes.go
@@ -2,18 +2,14 @@ package routes
 
 import (
 	"github.com/gin-gonic/gin"
-	"github.com/qulDev/jwt-gin-gorm/internal/handler"
 )
-
-type Handlers struct {
-	User *handler.UserHandler
-}
 
 func SetupRoutes(h *Handlers) *gin.Engine {
 	r := gin.Default()
 	v1 := r.Group("/api/v1")
 
 	AuthRoutes(v1, h.User)
+	ArticleRoute(v1, h.Article)
 
 	return r
 }
