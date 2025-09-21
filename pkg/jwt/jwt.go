8a2fd0ed0d30e575b1e7d10a7390f29df230ee00
qulDev <mm.rizzqullah@gmail.com>
2025-09-21 20:09:22 +0800

    feat: add JWT token generation function with claims support

---
+++ pkg/jwt/jwt.go
@@ -0,0 +1,37 @@
+package jwthelper
+
+import (
+	"time"
+
+	"github.com/golang-jwt/jwt/v5"
+	"github.com/qulDev/jwt-gin-gorm/internal/config"
+)
+
+type Claims struct {
+	ID   string `json:"id"`
+	Role string `json:"role"`
+	jwt.RegisteredClaims
+}
+
+func GenerateAccessToken(id, role string, ttl time.Duration) (string, error) {
+	secret := config.GetJWTSecret()
+
+	claims := Claims{
+		ID:   id,
+		Role: role,
+		RegisteredClaims: jwt.RegisteredClaims{
+			Subject:   id,
+			IssuedAt:  jwt.NewNumericDate(time.Now()),
+			ExpiresAt: jwt.NewNumericDate(time.Now().Add(ttl)),
+		},
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodES256, claims)
+
+	signedToken, err := token.SignedString([]byte(secret))
+	if err != nil {
+		return "", err
+	}
+
+	return signedToken, nil
+}
